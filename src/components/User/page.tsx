59249c7929bccbc14eedd8d220476a742961325b
husnain3964 <husnain3964@gmail.com>
2026-02-13 20:01:53 +0500

    feature/clientFomIntegrated

--- src/components/User/page.tsx
+++ src/components/User/page.tsx
@@ -67,9 +67,9 @@ export default function UserForm({
       formData.append("birthDate", data.birthDate);
       formData.append("emergencyContactNumber", data.emergencyContactNumber);
 
-      if (data.facePicture) {
-        formData.append("facePicture", data.facePicture  as File )     ;
-      }
+    if (data.facePicture && data.facePicture[0]) {
+  formData.append("facePicture", data.facePicture[0]);
+}
 
       const response = await Api.post(urls.client.create, formData);
 
